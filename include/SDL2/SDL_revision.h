9e3cb9abab2cee9d90dc0dd05b36388fa2d91310
Nicolas Morales <n.morales.0@proton.me>
2024-11-01 16:56:52 -0400

    use SDL 2.0.18 install from source

--- include/SDL2/SDL_revision.h
+++ include/SDL2/SDL_revision.h
@@ -1,2 +1,6 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@2e9821423a237a1206e3c09020778faacfe430be"
+/* #undef SDL_REVISION */
 #define SDL_REVISION_NUMBER 0
+
+#ifndef SDL_REVISION
+#define SDL_REVISION ""
+#endif
